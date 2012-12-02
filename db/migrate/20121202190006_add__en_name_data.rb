329598e6724a44ea3c71fccfd7989f870253d826
Jason <jason.addie@gmail.com>
2012-12-02 23:28:17 +0400

    now using correct syntax for find_in_batches

--- db/migrate/20121202190006_add__en_name_data.rb
+++ db/migrate/20121202190006_add__en_name_data.rb
@@ -2,22 +2,26 @@ class Add_enNameData < ActiveRecord::Migration
 	require 'utf8_converter'
   def up
     puts "starting to add english to names"
-    Name.find_in_batches(:batch_size => 100) do |name|
-      name.name_en = Utf8Converter.convert_ka_to_en(name[:name])
-      name.save
+    Name.find_in_batches(:batch_size => 100) do |names|
+      names.each do |name|
+        name.name_en = Utf8Converter.convert_ka_to_en(name[:name])
+        name.save
+      end
     end
     puts "finished adding english to names"
 
     puts "starting to add english to districts"
-    DistrictName.find_in_batches(:batch_size => 100) do |name|
-      name.name_en = Utf8Converter.convert_ka_to_en(name[:name])
-      name.save
+    DistrictName.find_in_batches(:batch_size => 100) do |names|
+      names.each do |name|
+        name.name_en = Utf8Converter.convert_ka_to_en(name[:name])
+        name.save
+      end
     end
     puts "finished adding english to districts"
   end
 
   def down
-    Name.all.update_all(:name_en => nil)
-    DistrictName.all.update_all(:name_en => nil)
+    Name.update_all(:name_en => nil)
+    DistrictName.update_all(:name_en => nil)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121202190006) do
+ActiveRecord::Schema.define(:version => 20121202181125) do
 
   create_table "birth_years", :force => true do |t|
     t.integer  "name_id"
