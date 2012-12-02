9ba10ef25015af29f75670ae7e21b7421d8b7a6b
Jason <jason.addie@gmail.com>
2012-12-02 23:02:49 +0400

    split add fields from adding data

--- db/migrate/20121202181125_add_en_names.rb
+++ db/migrate/20121202181125_add_en_names.rb
@@ -1,21 +1,12 @@
 class AddEnNames < ActiveRecord::Migration
-	require 'utf8_converter'
   def up
+=begin
     add_column :names, :name_en, :string
     add_index :names, :name_en
 
     add_column :district_names, :name_en, :string
     add_index :district_names, :name_en
-
-    Name.all.each do |name|
-      name.name_en = Utf8Converter.convert_ka_to_en(name[:name])
-      name.save
-    end
-
-    DistrictName.all.each do |name|
-      name.name_en = Utf8Converter.convert_ka_to_en(name[:name])
-      name.save
-    end
+=end
   end
 
   def down
---
+++ db/migrate/20121202190006_add__en_name_data.rb
@@ -0,0 +1,23 @@
+class Add_enNameData < ActiveRecord::Migration
+	require 'utf8_converter'
+  def up
+    puts "starting to add english to names"
+    Name.all.each do |name|
+      name.name_en = Utf8Converter.convert_ka_to_en(name[:name])
+      name.save
+    end
+    puts "finished adding english to names"
+
+    puts "starting to add english to districts"
+    DistrictName.all.each do |name|
+      name.name_en = Utf8Converter.convert_ka_to_en(name[:name])
+      name.save
+    end
+    puts "finished adding english to districts"
+  end
+
+  def down
+    Name.all.update_all(:name_en => nil)
+    DistrictName.all.update_all(:name_en => nil)
+  end
+end
